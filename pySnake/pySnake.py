7d02f7324ebe591b4a6ec7cad256bb0c75406145
Patrick Henson <patrick.henson@outlook.com>
2016-01-31 14:40:06 -0500

    add pySnake.py with main loop to display window

---
+++ pySnake/pySnake.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+from Tkinter import Tk, Frame, BOTH
+
+class PySnakeFrame(Frame):
+    def __init__(self, parent):
+        Frame.__init__(self, parent, background="black")
+        self.parent = parent
+        self.initUI()
+
+    def initUI(self):
+        self.parent.title("pySnake")
+        self.pack(fill=BOTH, expand=1)
+
+def main():
+    root = Tk()
+    root.geometry("640x480+300+300")
+    app = PySnakeFrame(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
