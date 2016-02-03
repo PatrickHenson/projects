8121be323bc0c41431f441e40edc53628c80a820
Patrick Henson <patrick.henson@outlook.com>
2016-02-02 23:08:35 -0500

    fix bugs: render blocks inside margins random block coordinates multiple of 5 cleanup moving functionality to appropriate classes

--- pySnake/pySnake.py
+++ pySnake/pySnake.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-
 from Tkinter import *
 import random
 
@@ -19,6 +18,7 @@ MAX_X       = 640
 MAX_Y       = 480
 BLOCK_SIZE  = 5
 
+# debug function prints string in terminal while application runs
 def DEBUG(message):
     print message
 
@@ -35,14 +35,14 @@ class PySnakeApp(Frame):
         # setup application window
         self.parent.title("pySnake")
         self.pack(fill=BOTH, expand=1)
-        # setup canvas to draw
+        # setup canvas to draw objects
         self.canvas = Canvas(self, background="black")
         self.canvas.pack(fill=BOTH, expand=True)
         # initialize snake
         self.snake = Snake(self.canvas)
 	# initialize bites
 	self.bites = Bites(self.canvas)
-        # bind keys
+        # bind key events to key bindings
         self.parent.bind('<Up>', self.moveUp)
         self.parent.bind('<Down>', self.moveDown)
         self.parent.bind('<Left>', self.moveLeft)
@@ -50,73 +50,95 @@ class PySnakeApp(Frame):
         # run application
         self.runApp()
 
-    # run application
+    # run pySnake application
     def runApp(self):
-	# move snake
+	    # move snake
         self.snake.moveSnake()
-	# check if bite eaten by snake head
-	if (self.bites.gotBite(self.snake.getHead())):
-	    self.snake.growSnake()
-	# check for collisions (end game)        
-	elif (self.snake.collisionDetected()):
+	    # check if bite eaten by snake head
+        if (self.bites.gotBite(self.snake.getHead())):
+            self.snake.growSnake()
+        # check for collisions (end game)        
+        elif (self.snake.collisionDetected()):
             return
         self.canvas.after(DELAY, self.runApp)
 
-    # key bindings
+    # key event bindings used to control snake movement
     def moveUp(self, event):
-        self.snake.setDirection(UP)
+        self.snake.moveUp()
     def moveDown(self, event):
-        self.snake.setDirection(DOWN)
+        self.snake.moveDown()
     def moveLeft(self, event):
-        self.snake.setDirection(LEFT)
+        self.snake.moveLeft()
     def moveRight(self, event):
-        self.snake.setDirection(RIGHT)
+        self.snake.moveRight()
 
-# class defining snake
+# class defining snake functionality
+# snake is created using linked list of blocks
 class Snake:
     def __init__(self, canvas):
         initX = 20
         initY = (MAX_Y - MIN_Y) / 2
         self.canvas = canvas
-        self.head = Block(canvas, initX, initY, "red")
-        self.head.setDirection(RIGHT)
+        self.direction = RIGHT
+        self.head = Block(canvas, initX, initY, "red")		
 
+    # return the head block
     def getHead(self):
-	return self.head
+        return self.head
 
     # add block to tail of snake
     def growSnake(self):
-	tail = self.head
-	while (tail.getNextBlock() != None):
-    	    tail = tail.getNextBlock()
-	newBlock = Block(self.canvas, tail.getX(), tail.getY(), "red")
-	tail.setNextBlock(newBlock)
+        tail = self.head
+        while (tail.getNextBlock() != None):
+            tail = tail.getNextBlock()
+        newBlock = Block(self.canvas, tail.getX(), tail.getY(), "red")
+        tail.setNextBlock(newBlock)
 
-    # move each block in snake
+    # functions controlling snake movement
+    def moveUp(self):
+        if (self.direction != DOWN):
+            self.direction = UP
+    def moveDown(self):
+        if (self.direction != UP):
+            self.direction = DOWN
+    def moveLeft(self):
+        if (self.direction != RIGHT):
+            self.direction = LEFT
+    def moveRight(self):
+        if (self.direction != LEFT):
+            self.direction = RIGHT
+
+    # move snake
     def moveSnake(self):
-	# chase head by shifting to prior blocks position
-	b = self.head
-	x = b.getX()
-        y = b.getY()
-	while (b.getNextBlock() != None):
-            n = b.getNextBlock()
-            tmpX = n.getX()
-	    tmpY = n.getY()
-            n.setCoordinates(x, y)
-            x = tmpX
-            y = tmpY
-	    b = n	    
-	# move head in stored direction
-	self.head.moveBlock()
+		# move each block takes position of previous block
+		b = self.head
+		x = b.getX()
+		y = b.getY()
+		while (b.getNextBlock() != None):
+			n = b.getNextBlock()
+			tmpX = n.getX()
+			tmpY = n.getY()
+			n.setCoordinates(x, y)
+			x = tmpX
+			y = tmpY
+			b = n	    
+		# move head in stored direction
+		x = self.head.getX()
+		y = self.head.getY()
+		if (self.direction == UP):		y -= 10
+		elif (self.direction == DOWN):	y += 10
+		elif (self.direction == LEFT):	x -= 10
+		elif (self.direction == RIGHT): x += 10
+		self.head.setCoordinates(x, y)
 
     # check for collisions
     def collisionDetected(self):
         # self collision
         block = self.head
         while (block.getNextBlock() != None):
-	    block = block.getNextBlock()
-            if (block == self.head):       
-		return True
+            block = block.getNextBlock()
+            if (block == self.head):
+                return True
         # wall collision
         if (self.head.getX() == MIN_X or
             self.head.getX() == MAX_X or
@@ -126,11 +148,7 @@ class Snake:
         # no collision
         return False
 
-    # only set the head block direction
-    def setDirection(self, direction):
-        self.head.setDirection(direction)
-
-# class storing random bites for snake to eat
+# class defining bites on map for snake to eat
 class Bites:
     def __init__(self, canvas):
 	self.canvas = canvas
@@ -141,53 +159,53 @@ class Bites:
 
     # add random bite to list
     def addBite(self):
-        x = random.randint(MIN_X, MAX_X)
-        y = random.randint(MIN_Y, MAX_Y)
+        minX = MIN_X + 10
+        maxX = (MAX_X - 10) / 5
+        minY = MIN_Y + 10
+        maxY = (MAX_Y - 10) / 5
+        # require generated point to be multiple of 5
+        x = random.randint(minX, maxX) * 5
+        y = random.randint(minY, maxY) * 5
         self.bites.append(Block(self.canvas, x, y, "white"))
 
-    # check if bite captured
+    # check for collition with snake head
     def gotBite(self, head):
-	bite = [x for x in self.bites if x == head]
-	if (len(bite) == 1):
-	    bite[0].removeBlock()
-	    self.addBite()
-            return True
-        return False
-
-#	self.bites[:] = [x for x in self.bites if not x == head]
-#	if (len(self.bites) < self.count):
-#	    self.addBite()
-#	    return True
-#	return False
+        detected = False
+        # return list containing collisions and remove from canvas
+        removeBites = [x for x in self.bites if x == head]
+        for b in removeBites:
+            b.removeBlock()
+            self.addBite()
+            detected = True
+        # remove block objects from current bite list
+        self.bites[:] = [x for x in self.bites if not x == head]
+        return detected
 	
 # class defining blocks used to create snake and bites
 class Block:
     def __init__(self, canvas, x, y, color):
         self.canvas = canvas
-        self.direction = -1
+        # allow blocks to form linked list to create snake
         self.nextBlock = None
-        # x,y coordinates for block center
+        # block attributes
+        self.size = 5
         self.x = x
         self.y = y
-        self.id = self.canvas.create_rectangle(x-5,y-5,x+5,y+5,fill=color)
+        size = self.size
+        self.id = self.canvas.create_rectangle(x-size,y-size,x+size,y+size,fill=color)
 
+	# return block's current x coordinate
     def getX(self):
         return self.x
 
+	# return block's current y coordinate
     def getY(self): 
         return self.y
 
     # overload eq function to detect if two blocks have collided
-    def __eq__(self, other):    
-        return (abs(self.x - other.x) <= 5 and abs(self.y - other.y) <= 5)
-
-    # set the block's direction of travel
-    def setDirection(self, direction):
-        self.direction = direction
-
-    # return the block's direction of travel
-    def getDirection(self):
-        return self.direction
+    def __eq__(self, other):
+        size = self.size  
+        return (abs(self.x - other.x) <= size and abs(self.y - other.y) <= size)
 
     # set linked block
     def setNextBlock(self, block):
@@ -202,29 +220,13 @@ class Block:
         # x,y coordinates for block center
         self.x = x
         self.y = y
-        self.canvas.coords(self.id,x-5,y-5,x+5,y+5)
-
-    # move the block in the appropriate direction
-    def moveBlock(self):
-	d = self.direction
-        shiftX = 0
-        shiftY = 0
-	if (d == UP):
-	    shiftY -= 10
-	elif (d == DOWN):
-	    shiftY += 10
-        elif (d == LEFT):
-            shiftX -= 10
-        elif (d == RIGHT):
-            shiftX += 10
-	self.x += shiftX
-	self.y += shiftY
-	self.setCoordinates(self.x, self.y)        
-	#self.canvas.move(self.id, shiftX, shiftY)
+        size = self.size
+        self.canvas.coords(self.id,x-size,y-size,x+size,y+size)
 
     def removeBlock(self):
-	self.canvas.delete(self.id)
+		self.canvas.delete(self.id)
 
+# main function to launch application
 def main():
     root = Tk()
     root.geometry("640x480+300+300")
@@ -233,3 +235,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
